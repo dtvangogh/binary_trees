2cdf150839e913f649156185a96fcf446bff16c9
dtvangogh <dt.van16@gmail.com>
2020-07-23 18:39:19 -0400

    all

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
-*CountNodesOnOneSide-calculates nodes in one side from initial root
+*countNodes - calculates nodes in one side from initial root
 *@tree: the node to start at
-*@nodes: node count
 *Return:size of one side of tree
 **/
 
 size_t countNodes(const binary_tree_t *tree)
 {
 	size_t nodes = 1;
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
@@ -29,6 +29,7 @@ size_t countNodes(const binary_tree_t *tree)
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+
 	size_t counter = 0;
 
 	if (tree == NULL)
