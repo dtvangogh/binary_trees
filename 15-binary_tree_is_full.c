54ca10cab7b93449a11a56c4e3b9b8c4dbd34139
dtvangogh <dt.van16@gmail.com>
2020-07-24 01:37:17 -0400

    all

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,6 +12,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!(tree))
 		return (0);
+	if (!(tree->right) || !(tree->left))
+		return (0);
 	if (!(tree->right) && !(tree->left))
 		return (1);
 	if (tree->left && tree->right)
