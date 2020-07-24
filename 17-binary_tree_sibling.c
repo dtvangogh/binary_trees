608b549fca0d662a6c17cc03ffeb67bfa7f151f8
dtvangogh <dt.van16@gmail.com>
2020-07-24 01:44:49 -0400

    all

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,11 +1,20 @@
 #include "binary_trees.h"
 
 /**
-**binary_tree_sibling-
-*
+**binary_tree_sibling-finds sibling of the node
+*@sibling: the node to check
 *
 *
 *Return:
 **/
 
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *binary_tree_sibling(binary_tree_t *sibling)
+{
+	if (!sibling)
+		return (NULL);
+	if (!(sibling->parent))
+		return (NULL);
+	if (sibling == sibling->parent->left)
+		return (sibling->parent->right);
+	return (sibling->parent->left);
+}
