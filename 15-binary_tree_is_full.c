1e97d4c9c15bc253b5bab5ee8f3e6026cc8eb75c
dtvangogh <dt.van16@gmail.com>
2020-07-24 01:34:52 -0400

    all

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,11 +1,20 @@
 #include "binary_trees.h"
 
 /**
-*binary_tree_is_full-
+*binary_tree_is_full-checks if tree is full
+*@tree: node to start at
 *
 *
-*
-*Return:
+*Return:0 or 1
 **/
 
 int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!(tree))
+		return (0);
+	if (!(tree->right) && !(tree->left))
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}
