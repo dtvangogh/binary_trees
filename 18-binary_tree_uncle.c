86ec2dbfccf7e83cc75c4b7e29fb7242ccf24529
dtvangogh <dt.van16@gmail.com>
2020-07-24 01:48:56 -0400

    all

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,11 +1,23 @@
 #include "binary_trees.h"
 
 /**
-**binary_tree_uncle-
+**binary_tree_uncle-returns ncle of the node
+*@node: node to look at
 *
 *
-*
-*Return:
+*Return:value of uncle, or nil if no uncle
 **/
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+	if (!(node->parent))
+		return (NULL);
+	if (!(node->parent->parent))
+		return (NULL);
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
+}
